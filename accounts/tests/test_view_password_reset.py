24bf5ff8170d66b154dc0ecbe563dc7a2208c1e5
ElimuStuart <stuartelimu@gmail.com>
2019-07-07 23:06:33 +0300

    added confirmation tests for password reset

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.shortcuts import reverse
+from django.contrib.auth.tokens import default_token_generator
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.core import mail
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 
 class PasswordResetTests(TestCase):
@@ -73,3 +76,37 @@ class PasswordResertDoneTests(TestCase):
     def test_view_function(self):
         view = resolve('/accounts/password_reset/done/')
         self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
+
+
+class PasswordResetConfirmTests(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='john', email='john@doe.com', password='123abcdef')
+        # create a valid password reset token
+        # based on how django creates the token internally:
+        # https://github.com/django/django/blob/2.2.1/django/contrib/auth/forms.py#L280
+        self.uid = urlsafe_base64_encode(force_bytes(user.pk))
+        self.token = default_token_generator.make_token(user)
+
+        url = reverse('password_reset_confirm', kwargs={'uidb64': self.uid, 'token': self.token})
+        self.response = self.client.get(url, follow=True)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/accounts/reset/{uidb64}/{token}/'.format(uidb64=self.uid, token=self.token))
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetConfirmView)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, SetPasswordForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain two inputs: csrf and two password fields
+        '''
+        self.assertContains(self.response, '<input', 3)
+        self.assertContains(self.response, 'type="password"', 2)
