0ae76818e1dc397c7e570beed9e2b0ef122f9040
ElimuStuart <stuartelimu@gmail.com>
2019-07-07 22:48:18 +0300

    added tests for completed password reset emails sent

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -60,3 +60,16 @@ class InvalidPasswordResetTests(TestCase):
 
     def test_no_reset_email_sent(self):
         self.assertEqual(0, len(mail.outbox))
+
+
+class PasswordResertDoneTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_done')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/accounts/password_reset/done/')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetDoneView)
