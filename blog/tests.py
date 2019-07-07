489b79b5b459a643113762c8e05b27b3f475bc9a
ElimuStuart <stuartelimu@gmail.com>
2019-07-07 15:14:59 +0300

    added index tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from django.urls import resolve
+from . import views
 
-# Create your tests here.
+class IndexTests(TestCase):
+    def test_index_view_status_code(self):
+        url = reverse('blog:index')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_index_url_resolves_index_view(self):
+        view = resolve('/mawazo/')
+        self.assertEquals(view.func.view_class, views.PostListView)
