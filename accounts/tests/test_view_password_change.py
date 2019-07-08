a06f44739941f1ab95ea7b4894a543aa98f9be43
ElimuStuart <stuartelimu@gmail.com>
2019-07-08 07:30:14 +0300

    tests for all password change views

--- accounts/tests/test_view_password_change.py
+++ accounts/tests/test_view_password_change.py
@@ -22,3 +22,42 @@ class PasswordChangeTestCase(TestCase):
         self.response = self.client.post(self.url, data)
 
 
+class SuccessfulPasswordChangeTests(PasswordChangeTestCase):
+    def setUp(self):
+        super().setUp({
+            'old_password': 'old_password',
+            'new_password1': 'new_password',
+            'new_password2': 'new_password',
+        })
+
+    def test_redirection(self):
+        # a valid form submission should redirect the user
+        self.assertRedirects(self.response, reverse('password_change_done'))
+
+    def test_password_changed(self):
+        # refresh the user instance from the database
+        # to get new password hash updated by the change password view
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('new_password'))
+
+    def test_user_authentication(self):
+        # Create a new request to an arbitrary page.
+        # The resulting response should now have an `user` to its context, after a successful sign up.
+        response = self.client.get(reverse('blog:index'))
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+    
+class InvalidPasswordChangeTests(PasswordChangeTestCase):
+    def test_status_code(self):
+        # an invalid form submission should return the same page
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_did_not_change_password(self):
+        # refresh user instance from database to make sure we have the latest data
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('old_password'))
