9d7e544855216c9a75f621705471917ce6bdff83
ElimuStuart <stuartelimu@gmail.com>
2019-11-12 22:44:58 +0300

    add Procfile file for deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mawazo.wsgi --log-file -
--- mawazo/settings.py
+++ mawazo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config, Csv
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,5 +157,7 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/mawazo/'
 
 LOGOUT_REDIRECT_URL = '/mawazo/'
+
+django_heroku.settings(locals())
 
 
